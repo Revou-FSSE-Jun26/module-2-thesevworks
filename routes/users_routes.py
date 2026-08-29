6de1edfd56e2131b970395f6669e915f2a834797
thesevworks <pocosevmi@gmail.com>
2026-08-29 08:05:19 +0700

    Add get user by id

--- routes/users_routes.py
+++ routes/users_routes.py
@@ -4,12 +4,24 @@ from flask import Blueprint, jsonify
 from flask_jwt_extended import jwt_required 
 
 users_bp = Blueprint('users', __name__)
+
 @users_bp.route("/", methods=["GET"]) 
 # @jwt_required() 
 # @admin_required 
 def get_users(): 
-    users = User.query. all()
+    users = User.query.all()
 
     return jsonify([ 
         user.to_dict() 
         for user in users ]), 200
+
+@users_bp.route("/<int:user_id>", methods=["GET"])
+def get_user_by_id(user_id):
+    try:
+        user = User.query.get(user_id)
+        if user:
+            return jsonify(user.to_dict()), 200
+        else:
+            return jsonify({"error": "User not found"}), 404
+    except Exception as e : 
+        return jsonify({"error": str(e)}), 500
