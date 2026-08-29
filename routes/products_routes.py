5a28e67e4c19799959c4238f2db5c7d71a17af8b
thesevworks <pocosevmi@gmail.com>
2026-08-29 08:04:59 +0700

    Add Get Product by id feature

--- routes/products_routes.py
+++ routes/products_routes.py
@@ -8,8 +8,42 @@ products_bp = Blueprint('products', __name__)
 # @jwt_required() 
 # @admin_required 
 def get_products(): 
-    products = Product.query. all()
-    
+    products = Product.query.all()
     return jsonify([ 
         product.to_dict() 
     for product in products ]), 200
+
+@products_bp.route("/<int:product_id>", methods=["GET"])
+def get_product_by_id(product_id):
+    try:
+        product = Product.query.get(product_id)
+        if not product:
+            return jsonify({"error": "Product not found"}), 404
+        return jsonify(product.to_dict()), 200        
+    except Exception as e : 
+        return jsonify({"error": str(e)}), 500
+
+# @product_bp.routes("/products", method=["POST"])
+# def create_product():
+#         data = request.get_json ()
+#         try
+#             new_product = Product(
+#                 name=data['name'],
+#                 description=data['description'],
+#                 price=data['price'],
+#                 stock=data['stock']
+#             )
+#             db.session.add(new_product)
+#             db.session.commit()
+#             return jsonify("message" : "new_product created",
+#                             "product": new_product.to_dict(),
+#                             "status": success),
+#                              201
+#         except Exception as e:
+#             db.session.rollback()
+#             print(f"Error creating product: {e}")
+#             return jsonify({"message": "Error creating product",
+#                             "product": "Not valid",
+#                             "status": "error"}),
+#                              400
+
